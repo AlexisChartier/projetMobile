16de2b3c9146627638ebfc86b4453702ebf08eea
samylouchahi <samy.louchahi@etu.umontpellier>
2024-03-22 13:22:19 +0100

    Initial Commit

---
+++ Projet mobile/Projet_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_mobileApp.swift
+//  Projet mobile
+//
+//  Created by Samy Louchahi on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Projet_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
